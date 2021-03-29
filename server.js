362020985564bd6afca693d7695bc16ce504e9f8
kantc4418 <chandra4418@gmail.com>
2021-03-29 23:10:30 +0530

    added getQuestions api

--- server.js
+++ server.js
@@ -27,42 +27,53 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-  app.post('/registerUser', function (req, res) {
-    con.query(
-      'INSERT INTO signup (name, email, password) VALUES (?,?,?)',
-      [req.body.name, req.body.email, req.body.password],
-      (error) => {
-        if (error) {
-          console.error(error);
-          res.status(500).json({status: 'error'});
+app.get('/getQuestions', function (req, res) {
+    con.query('SELECT * FROM questions', (err, result, fields) => {
+        if (err) {
+            throw err;
         } else {
-          res.status(200).json({status: 'user has been registered successfully'});
+            res.send(result);
         }
-      }
-    );
-   })
-  
-   app.post('/loginUser', function (req, res) {
-      var formemail= req.body.email;
-      var formpassword = req.body.password;
-      if (formemail && formpassword) {
-      con.query('SELECT * FROM signup WHERE email = ? AND password = ?', [formemail, formpassword], function(error, results, fields) {
-        if (results.length > 0) {
-                  // res.status(200).json({status: 'success'});
-                  var token = jwt.sign({ id: results.id }, "chandra4418", {
-                    expiresIn: 86400 // expires in 24 hours
-                  });
-                  res.status(200).send({ status: 'success', auth: true, token: token });
-                  // res.send('user logged in successgully');
-        } else {
-                  res.status(500).json({status: 'error'});
-          // res.send('Incorrect Username and/or Password!');
-        }     
-        res.end();
-      });
-    } else {
-      res.send('Please enter Username and Password!');
-      res.end();
     }
+   );
+  })
+
+app.post('/registerUser', function (req, res) {
+con.query(
+    'INSERT INTO signup (name, email, password) VALUES (?,?,?)',
+    [req.body.name, req.body.email, req.body.password],
+    (error) => {
+    if (error) {
+        console.error(error);
+        res.status(500).json({status: 'error'});
+    } else {
+        res.status(200).json({status: 'user has been registered successfully'});
+    }
+    }
+);
+})
+  
+app.post('/loginUser', function (req, res) {
+    var formemail= req.body.email;
+    var formpassword = req.body.password;
+    if (formemail && formpassword) {
+    con.query('SELECT * FROM signup WHERE email = ? AND password = ?', [formemail, formpassword], function(error, results, fields) {
+    if (results.length > 0) {
+                // res.status(200).json({status: 'success'});
+                var token = jwt.sign({ id: results.id }, "chandra4418", {
+                expiresIn: 86400 // expires in 24 hours
+                });
+                res.status(200).send({ status: 'success', auth: true, token: token });
+                // res.send('user logged in successgully');
+    } else {
+                res.status(500).json({status: 'error'});
+        // res.send('Incorrect Username and/or Password!');
+    }     
+    res.end();
+    });
+} else {
+    res.send('Please enter Username and Password!');
+    res.end();
+}
 })
   
