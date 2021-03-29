88bab863787a41631a5e2806470ab253f570818e
kantc4418 <chandra4418@gmail.com>
2021-03-29 17:13:02 +0530

    inital node project setup

---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var app = express();
+
+var cors = require('cors');
+const bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 3000;
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "emp_management"
+})
+
+con.connect();
+
+app.listen(PORT, function () {
+    console.log('Example app listening on port 3000!');
+  });
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+  app.post('/registerUser', function (req, res) {
+    con.query(
+      'INSERT INTO signup (name, email, password) VALUES (?,?,?)',
+      [req.body.name, req.body.email, req.body.password],
+      (error) => {
+        if (error) {
+          console.error(error);
+          res.status(500).json({status: 'error'});
+        } else {
+          res.status(200).json({status: 'user has been registered successfully'});
+        }
+      }
+    );
+   })
+  
+   app.post('/loginUser', function (req, res) {
+      var formemail= req.body.email;
+      var formpassword = req.body.password;
+      if (formemail && formpassword) {
+      con.query('SELECT * FROM signup WHERE email = ? AND password = ?', [formemail, formpassword], function(error, results, fields) {
+        if (results.length > 0) {
+                  // res.status(200).json({status: 'success'});
+                  var token = jwt.sign({ id: results.id }, "chandra4418", {
+                    expiresIn: 86400 // expires in 24 hours
+                  });
+                  res.status(200).send({ status: 'success', auth: true, token: token });
+                  // res.send('user logged in successgully');
+        } else {
+                  res.status(500).json({status: 'error'});
+          // res.send('Incorrect Username and/or Password!');
+        }     
+        res.end();
+      });
+    } else {
+      res.send('Please enter Username and Password!');
+      res.end();
+    }
+})
+  
