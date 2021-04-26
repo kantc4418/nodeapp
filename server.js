2000ca6df4e8252443644de010fb152bf21fc5fe
kantc4418 <chandra4418@gmail.com>
2021-04-26 15:34:45 +0530

    added code

--- server.js
+++ server.js
@@ -3,8 +3,28 @@ var app = express();
 
 var jwt = require('jsonwebtoken');
 
+const multer = require('multer');
+
+const readXlsxFile = require('read-excel-file/node');
+
 var cors = require('cors');
-const bodyParser = require('body-parser');
+
+var xlsxtojson = require("xlsx-to-json");
+var xlstojson = require("xls-to-json");
+
+global.__basedir = __dirname;
+
+app.use(function(req, res, next) { //allow cross origin requests
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Max-Age", "3600");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    next();
+});
+// configuration
+app.use(express.static(__dirname + '/public')); 
+
+app.use('/public/uploads',express.static(__dirname + '/public/uploads'));   
 
 var PORT = process.env.PORT || 3000;
 
@@ -19,13 +39,61 @@ var con = mysql.createConnection({
 
 con.connect();
 
+// -> Multer Upload Storage
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+	   cb(null, __basedir + '/uploads/')
+	},
+	filename: (req, file, cb) => {
+	   cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname)
+	}
+});
+
+const upload = multer({storage: storage});
+
+// -> Express Upload RestAPIs
+app.post('/api/uploadfile', upload.single("uploadfile"), (req, res) =>{
+	importExcelData2MySQL(__basedir + '/uploads/' + req.file.filename);
+	res.json({
+				'msg': 'File uploaded/import successfully!', 'file': req.file
+			});
+});
+
+// -> Import Excel Data to MySQL database
+function importExcelData2MySQL(filePath){
+	// File path.
+	readXlsxFile(filePath).then((rows) => {
+		// `rows` is an array of rows
+		// each row being an array of cells.	 
+		console.log(rows);
+	 
+		/**
+		[ [ 'Id', 'Name', 'Address', 'Age' ],
+		[ 1, 'Jack Smith', 'Massachusetts', 23 ],
+		[ 2, 'Adam Johnson', 'New York', 27 ],
+		[ 3, 'Katherin Carter', 'Washington DC', 26 ],
+		[ 4, 'Jack London', 'Nevada', 33 ],
+		[ 5, 'Jason Bourne', 'California', 36 ] ] 
+		*/
+	 
+		// Remove Header ROW
+		rows.shift();
+
+	 	let query = 'INSERT INTO questions (qnId, question, option1, option2, option3, option4, actual_answer, user_answer) VALUES ?';
+		con.query(query, [rows], (error, response) => {
+		console.log(error || response);
+				});
+  });
+}
+
+
 app.listen(PORT, function () {
     console.log('Example app listening on port 3000!');
   });
 
 app.use(cors());
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
@@ -39,12 +107,37 @@ function authenticateToken(req, res, next) {
     
   }
 
+  app.post('/api/xlstojson', function(req, res) {
+    xlsxtojson({
+        input: "./sample.xlsx",  // input xls
+        output: "output.json", // output json
+        lowerCaseHeaders:true
+    }, function(err, result) {
+        if(err) {
+          res.json(err);
+        } else {
+          res.json(result);
+        }
+    });
+});
+
 app.get('/getQuestions', authenticateToken, function (req, res) {
     con.query('SELECT * FROM questions', (err, result, fields) => {
         if (err) {
             throw err;
         } else {
-            res.send(result);
+            res.send({data: result, totalQues: result.length});
+        }
+    }
+   );
+  })
+
+  app.get('/getPartcipants', function (req, res) {
+    con.query('SELECT * FROM signup', (err, result, fields) => {
+        if (err) {
+            throw err;
+        } else {
+            res.send({data: result, totalParticipant: result.length});
         }
     }
    );
@@ -70,22 +163,22 @@ app.post('/loginUser', function (req, res) {
     var formpassword = req.body.password;
     if (formemail && formpassword) {
     con.query('SELECT * FROM signup WHERE email = ? AND password = ?', [formemail, formpassword], function(error, results, fields) {
-    if (results.length > 0) {
+      if (results.length > 0) {
                 // res.status(200).json({status: 'success'});
-                var token = jwt.sign({ id: results }, "chandra4418", {
-                expiresIn: 86400 // expires in 24 hours
+                var token = jwt.sign({ id: results.id }, "chandra4418", {
+                  expiresIn: 86400 // expires in 24 hours
                 });
                 res.status(200).send({ status: 'success', auth: true, token: token });
                 // res.send('user logged in successgully');
-    } else {
+      } else {
                 res.status(500).json({status: 'error'});
         // res.send('Incorrect Username and/or Password!');
-    }     
-    res.end();
+      }     
+      res.end();
     });
-} else {
+  } else {
     res.send('Please enter Username and Password!');
     res.end();
-}
-})
+  }
+   })
   
